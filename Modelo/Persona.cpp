e74345840b145968427ea64d22fc332e9f3e7630
Juan Sebastian Valencia <juan.quinchua@correounivalle.edu.co>
2025-11-16 16:19:50 -0500

    definimos e implementamos la clase Persona

---
+++ Modelo/Persona.cpp
@@ -0,0 +1,17 @@
+#include "Persona.h"
+
+Persona::Persona() : nombre(""), edad(0), puntaje(0), saldo(0.0), apuesta(0.0) {}
+
+// Getters
+std::string Persona::getNombre() const { return nombre;}
+int Persona::getEdad() const { return edad;}
+int Persona::getPuntaje() const { return puntaje;}
+double Persona::getSaldo() const { return saldo;}
+double Persona::getApuesta() const { return apuesta;}
+
+// Setters
+void Persona::setNombre(const std::string& nombre){this->nombre = nombre;}
+void Persona::setEdad(int edad) {this -> edad = edad;}
+void Persona::setPuntaje(int puntaje) {this -> puntaje = puntaje;}
+void Persona::setSaldo(double saldo) {this -> saldo = saldo;}
+void Persona::setApuesta(double apuesta) {this -> apuesta = apuesta;}
--- Modelo/Persona.h
+++ Modelo/Persona.h
@@ -4,9 +4,45 @@
 ===============================================================================
 Nombre de la clase:       Persona
 Responsabilidades:        
-    ...
+    - Representar a una persona en un cacino con atributos básicos como nombre,
+      edad, puntaje saldo y apuesta.
+    - Crear métods getters y setters para accceder y modificar los atributos.
+    - Clase padre de Jugador y Crupier.
 
 Colaboradores:            
-    ...
+    - Hereda a Jugador.
+    - Hereda a crupier.
 ===============================================================================
 */
+
+#ifndef MODELO_PERSONA_H
+#define MODELO_PERSONA_H
+
+#include <string>
+
+class Persona {
+    private:
+        std::string nombre;
+        int edad;
+        int puntaje;
+        double saldo;
+        double apuesta;
+    
+    public:
+        Persona();
+        std::string getNombre() const;
+        int getEdad() const;
+        int getPuntaje() const;
+        double getSaldo() const;
+        double getApuesta() const;
+
+        void setNombre(const std::string& nombre);
+        void setEdad(int edad);
+        void setPuntaje(int puntaje);
+        void setSaldo(double saldo);
+        void setApuesta(double apuesta);
+};
+
+
+
+#endif // MODELO_PERSONA_H
